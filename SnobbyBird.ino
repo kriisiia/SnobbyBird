9b7fccdc0e43fb31eb3f451f29764d3229821a00
kriisiia <krisia.ayala@gmail.com>
2014-12-09 20:08:10 -0500

    plays 2 files based upon if statement

--- SnobbyBird.ino
+++ SnobbyBird.ino
@@ -31,16 +31,17 @@ void setup() {
   pinMode(outermouthleds, OUTPUT);
   pinMode(midmouthleds, OUTPUT);
   pinMode(mouthleds, OUTPUT);
-  
+
   randomSeed(analogRead(0));
 
 
   if (!card.init()) {
-    Serial.println(F("Card init. failed!")); return;
+    Serial.println(F("Card init. failed!")); 
+    return;
   }
   // enable optimize read - some cards may timeout. Disable if you're having problems
   card.partialBlockRead(true);
-  
+
   // Now we will look for a FAT partition!
   uint8_t part;
   for (part = 0; part < 5; part++) {   // we have up to 5 slots to look in
@@ -50,18 +51,18 @@ void setup() {
   if (part == 5) {                     // if we ended up not finding one  :(
     Serial.println(F("No valid FAT partition!"));  // Something went wrong, lets print out why
   }
-  
+
   // Lets tell the user about what we found
   putstring("Using partition ");
   Serial.print(part, DEC);
   Serial.print(F(", type is FAT"));
   Serial.println(vol.fatType(), DEC);     // FAT16 or FAT32?
-  
+
   // Try to open the root directory
   if (!root.openRoot(vol)) {
     Serial.println(F("Can't open root dir!"));      // Something went wrong,
   }
-  
+
   // Whew! We got past the tough parts.
   Serial.println(F("Files found (* = fragmented):"));
 
@@ -71,112 +72,136 @@ void setup() {
 
 
 void pulseServo(uint8_t servopin, uint16_t p) {
-  
- digitalWrite(servopin, HIGH);
- delayMicroseconds(600);
- while (p--) {
-   delayMicroseconds(4);
- }
- digitalWrite(servopin, LOW);
+
+  digitalWrite(servopin, HIGH);
+  delayMicroseconds(600);
+  while (p--) {
+    delayMicroseconds(4);
+  }
+  digitalWrite(servopin, LOW);
   delay(18);
 }
 
 uint8_t pumpkinstate = 0;
 
 void loop() { 
-   int distsensor, i;
-   long time;
-   /*
+  int distsensor, i;
+  long time;
+  /*
    for (i=0; i<50; i++) {
-     pulseServo(servo,0);
+   pulseServo(servo,0);
    }
    for (i=0; i<50; i++) {
-     pulseServo(servo,400);
+   pulseServo(servo,400);
    }
    return;
    */
-   distsensor = 0;
-   for (i=0; i<8; i++) {
-     distsensor += analogRead(0);
-     delay(50);
-   }
-   distsensor /= 8;
+  distsensor = 0;
+  for (i=0; i<8; i++) {
+    distsensor += analogRead(0);
+    delay(50);
+  }
+  distsensor /= 8;
 
-   Serial.print(F("Sensor = ")); Serial.println(distsensor);
-   
-   if (distsensor <= 500) {
-     digitalWrite(eyeleds, HIGH); 
-   } 
-   if (distsensor > 500) {
-     digitalWrite(eyeleds, LOW);  
-     pumpkinstate = 1;
-     // nobody there. one out of 200 times play one of the scary sounds (once every few minutes)
-     i = random(200);
-     //Serial.println(i);
-     if (i == 0) {
-       i = random(3);
-       if (i == 0) {
-           playcomplete("001.WAV");
-       } else if (i == 1) {
-           playcomplete("002.WAV");
-       } else {
-           playcomplete("003.WAV");   
-       }
-     }
-   } else if ((distsensor > 300) && (distsensor < 400)) {
-     if (pumpkinstate <= 1) {    // play "hello children"
-        playcomplete("004.WAV"); 
-     } else {
-       i = random(60);            // more often
-       //Serial.println(i);
-       if (i == 0) {
-         i = random(3);
-         if (i == 0) {
-           playcomplete("005.WAV");
-         } else if (i == 1) {
-           playcomplete("006.WAV");
-         } else {
-           playcomplete("007.WAV");   
-         }
-       } 
-     }
-     pumpkinstate = 2; 
-   } else if ((distsensor > 100) && (distsensor < 200)) {
-     if (pumpkinstate <= 2) {    // play "hello children"
-       playcomplete("008.WAV"); 
-     } else {
-       i = random(50);            // more often
-       //Serial.println(i);
-       if (i == 0) {
-         i = random(3);
-         if (i == 0) {
-           playcomplete("009.WAV");
-         } else if (i == 1) {
-           playcomplete("010.WAV");
-         } else {
-           playcomplete("011.WAV");   
-         }
-       }
-     }
-     pumpkinstate = 3;
-   } else if (distsensor < 50) {
-     if (pumpkinstate <= 3) {    // play "hello children"
-        playcomplete("012.WAV");    
-     } else {
-       i = random(30);            // more often
-     //Serial.println(i);
-     if (i == 0) {
-       i = random(2);
-       if (i == 0) {
-           playcomplete("013.WAV");
-       } else if (i == 1) {
-           playcomplete("014.WAV");
-       } 
-     }
-       
-   }
+  Serial.print(F("Sensor = ")); 
+  Serial.println(distsensor);
+
+
+  if (distsensor < 299 ){
+    playcomplete("004.WAV"); 
+  }
+
+  else if ((distsensor > 300) && (distsensor < 400)) {
+    playcomplete("003.WAV");   
+
+
+  }
+  if (distsensor <= 500) {
+    digitalWrite(eyeleds, HIGH); 
+  } 
+  if (distsensor > 500) {
+    digitalWrite(eyeleds, LOW);  
+    pumpkinstate = 1;
+    // nobody there. one out of 200 times play one of the scary sounds (once every few minutes)
+    i = random(200);
+    //Serial.println(i);
+    if (i == 0) {
+      i = random(3);
+      if (i == 0) {
+        playcomplete("001.WAV");
+      } 
+      else if (i == 1) {
+        playcomplete("002.WAV");
+      } 
+      else {
+        playcomplete("003.WAV");   
+      }
+    }
+  } 
+  else if ((distsensor > 300) && (distsensor < 400)) {
+    if (pumpkinstate <= 1) {    // play "hello children"
+      playcomplete("004.WAV"); 
+    } 
+    else {
+      i = random(60);            // more often
+      //Serial.println(i);
+      if (i == 0) {
+        i = random(3);
+        if (i == 0) {
+          playcomplete("005.WAV");
+        } 
+        else if (i == 1) {
+          playcomplete("006.WAV");
+        } 
+        else {
+          playcomplete("007.WAV");   
+        }
+      } 
+    }
+    pumpkinstate = 2; 
+  } 
+  else if ((distsensor > 100) && (distsensor < 200)) {
+    if (pumpkinstate <= 2) {    // play "hello children"
+      playcomplete("008.WAV"); 
+    } 
+    else {
+      i = random(50);            // more often
+      //Serial.println(i);
+      if (i == 0) {
+        i = random(3);
+        if (i == 0) {
+          playcomplete("009.WAV");
+        } 
+        else if (i == 1) {
+          playcomplete("010.WAV");
+        } 
+        else {
+          playcomplete("011.WAV");   
+        }
+      }
+    }
+    pumpkinstate = 3;
+  } 
+  else if (distsensor < 50) {
+    if (pumpkinstate <= 3) {    // play "hello children"
+      playcomplete("012.WAV");    
+    } 
+    else {
+      i = random(30);            // more often
+      //Serial.println(i);
+      if (i == 0) {
+        i = random(2);
+        if (i == 0) {
+          playcomplete("013.WAV");
+        } 
+        else if (i == 1) {
+          playcomplete("014.WAV");
+        } 
+      }
+
+    }
     pumpkinstate = 4;
- }
+  }
 }
 
 
@@ -185,7 +210,7 @@ void ROM_playcomplete(const char *romname) {
   char name[13], i;
   uint8_t volume;
   int v2;
-  
+
   for (i=0; i<13; i++) {
     name[i] = pgm_read_byte(&romname[i]);
   }
@@ -193,44 +218,52 @@ void ROM_playcomplete(const char *romname) {
   Serial.println(name);
   playfile(name);
   while (wave.isplaying) {
-   volume = 0;
-   for (i=0; i<8; i++) {
-     v2 = analogRead(1) - 512;
-     if (v2 < 0) 
+    volume = 0;
+    for (i=0; i<8; i++) {
+      v2 = analogRead(1) - 512;
+      if (v2 < 0) 
         v2 *= -1;
-     if (v2 > volume)
-       volume = v2;
-     delay(5);
-   }
-   if (volume > 200) {
-     digitalWrite(outermouthleds, HIGH);
-   } else {
-     digitalWrite(outermouthleds, LOW);
-   }
-   if (volume > 150) {
-     digitalWrite(midmouthleds, HIGH);
-   } else {
-     digitalWrite(midmouthleds, LOW);
-   } 
-   if (volume > 100) {
-     digitalWrite(mouthleds, HIGH);
-   } else {
-     digitalWrite(mouthleds, LOW);
-   } 
-   //Serial.print(F("vol = ")); Serial.println(volume, DEC);
+      if (v2 > volume)
+        volume = v2;
+      delay(5);
+    }
+    if (volume > 200) {
+      digitalWrite(outermouthleds, HIGH);
+    } 
+    else {
+      digitalWrite(outermouthleds, LOW);
+    }
+    if (volume > 150) {
+      digitalWrite(midmouthleds, HIGH);
+    } 
+    else {
+      digitalWrite(midmouthleds, LOW);
+    } 
+    if (volume > 100) {
+      digitalWrite(mouthleds, HIGH);
+    } 
+    else {
+      digitalWrite(mouthleds, LOW);
+    } 
+    //Serial.print(F("vol = ")); Serial.println(volume, DEC);
   }
   file.close();
 }
 
 void playfile(char *name) {
- // look in the root directory and open the file
+  // look in the root directory and open the file
   if (!file.open(root, name)) {
-    putstring("Couldn't open file "); Serial.print(name); return;
+    putstring("Couldn't open file "); 
+    Serial.print(name); 
+    return;
   }
-   if (!wave.create(file)) {
-     Serial.println(F(" Not a valid WAV")); return;
-   }
-   // ok time to play!
-   wave.play();
+  if (!wave.create(file)) {
+    Serial.println(F(" Not a valid WAV")); 
+    return;
+  }
+  // ok time to play!
+  wave.play();
 }
+
+
 
